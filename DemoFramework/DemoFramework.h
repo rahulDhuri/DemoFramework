981359f3cdf275783b2c8b73a7fb70f1150f76dd
Rahul Dhuri <rdhuri@athenahealth.com>
2018-07-11 17:47:42 +0530

    First commit

---
+++ DemoFramework/DemoFramework.h
@@ -0,0 +1,19 @@
+//
+//  DemoFramework.h
+//  DemoFramework
+//
+//  Created by Rahul Dhuri on 7/11/18.
+//  Copyright © 2018 Praxify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DemoFramework.
+FOUNDATION_EXPORT double DemoFrameworkVersionNumber;
+
+//! Project version string for DemoFramework.
+FOUNDATION_EXPORT const unsigned char DemoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoFramework/PublicHeader.h>
+
+
---
+++ DemoFramework/TestFile.swift
@@ -0,0 +1,9 @@
+//
+//  TestFile.swift
+//  DemoFramework
+//
+//  Created by Rahul Dhuri on 7/11/18.
+//  Copyright © 2018 Praxify. All rights reserved.
+//
+
+import Foundation
